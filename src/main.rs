536523c765a590e1905c919f9182c659ffa58c61
Jascha030 <contact@jaschavanaalst.nl>
2022-03-18 20:22:28 +0100

    🚧 Start app with tailwind.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use dioxus::prelude::*;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    
+    let app = Router::new()
+        .route("/", get(dioxusapp));
+
+    println!("listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn dioxusapp() -> Html<String> {
+    fn dioxusapp(cx: Scope) -> Element {
+        cx.render(rsx!(
+            head {
+                link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" }
+            }
+            body {
+                class: "w-full h-full bg-blue-500",
+                div {
+                }
+            }
+        ))
+    }
+    let mut app = VirtualDom::new(dioxusapp);
+    let _ = app.rebuild();
+    Html(dioxus::ssr::render_vdom(&app))
 }
